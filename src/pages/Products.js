b53764983c6f97d57942501ccef9e45fee97a544
Yashar Akish <yashar_akish@yahoo.com>
2022-02-03 21:22:42 -0500

    router introduction

---
+++ src/pages/Products.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+const Products = () => {
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li><Link to='/products/p1'>A Book</Link></li>
+        <li><Link to='/products/p2'>A Car</Link></li>
+        <li><Link to='/products/p3'>A TV Show</Link></li>
+      </ul>
+    </section> 
+    
+  )
+};
+
+export default Products;
